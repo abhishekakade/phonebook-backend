ed5e54708b260fab980018c304d2770bd67cc6c7
Abhishek K <abhishekakade@gmail.com>
2020-07-18 22:10:32 +0530

    uibuild

---
+++ build/precache-manifest.edd065e2ebb9cc32d503779919dacc7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "893a5bb0b16ac5e290959ab80d548683",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e119273a101d02e209ab",
+    "url": "/static/css/main.596f61e9.chunk.css"
+  },
+  {
+    "revision": "9a81b2ca0d2b256a2772",
+    "url": "/static/js/2.29946b2a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.29946b2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e119273a101d02e209ab",
+    "url": "/static/js/main.4a321d53.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
